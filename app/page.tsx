07a386868893daa6e914f7cd121aab2684dea9b6
harzeezco <wakilabdulazeez@gmail.com>
2024-06-21 21:05:12 +0100

    implement trusted companies section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Hero from '@/sections/hero';
+import TrustedCompanies from '@/sections/trusted-companies';
 
 const Homepage = () => (
   <main>
     <Hero />
+    <TrustedCompanies />
   </main>
 );
 
