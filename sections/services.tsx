0c89b04e642cd5db94b769e8d44eb6f61668d294
harzeezco <wakilabdulazeez@gmail.com>
2024-07-12 19:55:51 +0100

    Revert "adjust codebase and Ui"
    This reverts commit 1911ec7ad8518bf419115aaa636cd2bfeca0dfb9.

--- sections/services.tsx
+++ sections/services.tsx
@@ -8,8 +8,7 @@ import {
   useScroll,
   useTransform,
 } from 'framer-motion';
-
-/* eslint-disable jsx-a11y/media-has-caption */
+import Image from 'next/image';
 
 const Services = () => {
   const container = React.useRef<HTMLDivElement>(null);
@@ -18,7 +17,7 @@ const Services = () => {
   });
 
   const [isMobile, setIsMobile] = React.useState(false);
-  const [screen, setScreen] = React.useState('-76.4%');
+  const [screen, setScreen] = React.useState('-71.4%');
 
   const x = useTransform(scrollYProgress, [0, 1], ['1%', screen], {
     ease: cubicBezier(0.17, 0.67, 0.83, 0.67),
@@ -30,10 +29,6 @@ const Services = () => {
       setIsMobile(true);
     }
 
-    if (typeof window !== 'undefined' && window.innerWidth <= 1090) {
-      setScreen('-80.8%');
-    }
-
     if (typeof window !== 'undefined' && window.innerWidth >= 1300) {
       setScreen('-61.8%');
     }
@@ -42,7 +37,7 @@ const Services = () => {
   return (
     <section id='services'>
       <div ref={container} className='relative md:h-[300vh]'>
-        <div className='sticky left-0 top-5 flex items-center overflow-hidden md:h-screen'>
+        <div className='sticky left-0 top-10 flex items-center overflow-hidden md:h-screen 2xl:h-[60vh]'>
           <motion.div
             className='flex gap-4 max-md:flex-col'
             style={isMobile ? undefined : { x }}
@@ -51,38 +46,25 @@ const Services = () => {
               <div
                 key={title}
                 className={cn(
-                  'flex flex-col justify-center rounded-xl px-4  transition duration-200 h-[85vh] hover:bg-[#EFF7FF]',
-                  idx === 0 ? 'md:w-[250px]' : 'md:w-[600px]',
+                  'flex flex-col justify-center rounded-xl p-4 transition duration-200 h-[450px] hover:bg-[#EFF7FF]',
+                  idx === 0 ? 'md:w-[200px]' : 'md:w-[500px]',
                 )}
                 style={{ background: color }}
               >
                 {src && (
-                  <div className=''>
-                    <video
-                      autoPlay
-                      loop
-                      className='size-full rounded-3xl'
-                      height={100}
-                      width={100}
-                    >
-                      <source
-                        src={`/videos/${src}`}
-                        type='video/mp4'
-                      />
-                    </video>
+                  <div className='mb-4'>
+                    <Image
+                      alt=''
+                      className='h-[200px] w-full rounded-3xl'
+                      height={200}
+                      src={`/images/${src}`}
+                      width={450}
+                    />
                   </div>
                 )}
-                {idx === 0 ? (
-                  <div
-                    dangerouslySetInnerHTML={{ __html: title }}
-                    className='text-2xl font-semibold text-primary-600 sm:text-3xl lg:text-4xl'
-                  />
-                ) : (
-                  <h1 className='mb-2 text-2xl font-semibold text-primary-750 sm:text-3xl lg:text-4xl'>
-                    {title}
-                  </h1>
-                )}
-
+                <h1 className='mb-2 text-2xl font-semibold text-primary-750 sm:text-3xl lg:text-4xl'>
+                  {title}
+                </h1>
                 <p className='mt-3 text-2xl'>{desc}</p>
               </div>
             ))}
@@ -95,61 +77,61 @@ const Services = () => {
 
 export const SERVICES = [
   {
-    title: 'Why <br /> choose us',
+    title: 'Why choose us',
     color: '#fff',
   },
   {
     title: 'Medicare',
     desc: 'We offer personalized health insurance solutions, ensuring you get the best coverage for your needs.',
     color: '#FEECED',
-    src: '1.mp4',
+    src: 'family.webp',
   },
   {
     title: 'ACA - Obama Care',
     desc: 'Get access to affordable care through the ACA, ensuring coverage for all essential health benefits.',
     color: '#FEF8EC',
-    src: '2.mp4',
+    src: 'school.webp',
   },
   {
     title: 'FE – Final Expense',
     desc: 'Our final expense plans ensure your loved ones are financially protected during tough times.',
     color: '#FEEBFF',
-    src: '3.mp4',
+    src: 'aged.webp',
   },
   {
     icon: 'doc.svg',
     title: 'SSDI',
     desc: 'We assist you in applying for Social Security Disability Insurance, securing your financial future.',
     color: '#EEF9FC',
-    src: '4.mp4',
+    src: 'family.webp',
   },
   {
     icon: 'motor.svg',
     title: 'Auto Insurance',
     desc: 'Protect yourself and your vehicle with our reliable and comprehensive auto insurance policies.',
-    color: '#EEF9FC',
-    src: '5.mp4',
+    color: '#EDF1FD',
+    src: 'school.webp',
   },
   {
     icon: 'settle.svg',
     title: 'Debt Settlement',
     desc: 'Our debt settlement services help you manage and reduce your financial liabilities effectively.',
     color: '#FEF8EC',
-    src: '6.mp4',
+    src: 'aged.webp',
   },
   {
     icon: 'people.svg',
     title: 'U65 Health Insurance',
     desc: 'We offer specialized plans for those under 65, providing flexible and affordable coverage options.',
     color: '#DEFCF9',
-    src: '7.mp4',
+    src: 'family.webp',
   },
   {
     icon: 'tax.svg',
     title: 'Tax Debt',
     desc: 'Our experts help you navigate and resolve tax debt issues efficiently, easing your financial burden.',
-    color: '#D8DBDF',
-    src: '8.mp4',
+    color: '#FAF0F8',
+    src: 'school.webp',
   },
 ];
 
